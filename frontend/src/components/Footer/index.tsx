633d644acfae38efbc810c8fbe5e08265eff0735
Everton-Dantas <evertonbonfim@yahoo.com.br>
2021-05-03 22:00:20 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/everton-Dantas" target="_blank" rel="noreferrer">Everton Dantas</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/everton-b-dantas-8a970072/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+      
+      
+    );
+  }
+  
+  export default Footer;
